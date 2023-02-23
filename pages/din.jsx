f18b974f10c349a1f2e9aa58684b91138e0150c6
Korn <79989518+kornls@users.noreply.github.com>
2023-02-23 10:21:53 +0700

    Update din.jsx

--- pages/din.jsx
+++ pages/din.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
         <h1 className="font-semibold text-xl">About Me.</h1>
         <p className="max-w-lg">
           I&apos;m a tall black man <i>probably</i> from State of Western Australia (yes).
-          Being the Co-Founder of Larry Studios, I got a PhD in the Keynote University of Thailand <i>which is definitely a real university and not one I just made up two minutes ago</i> I have a 1600 FIDE
+          Being the Co-Founder of Larry Studios, I got a PhD in the Keynote University of Thailand <i>which is definitely a real university and not one I just made up two minutes ago</i>. I have a 1600 FIDE
           rating (trust me).
         </p>
       </main>
